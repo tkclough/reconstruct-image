a9ef385de87b9a3eb461c365eb81e66115f6dc25
Tim Clough <tmclough98@gmail.com>
2021-01-31 15:50:02 -0500

    make image live update

--- www/index.js
+++ www/index.js
@@ -2,10 +2,15 @@ import * as wasm from "reconstruct-image"
 
 let image_reconstruction;
 
-let originalCanvas = document.getElementById("original-image-canvas"),
-    grayscaleCanvas = document.getElementById("grayscale-image-canvas"),
-    corruptedCanvas = document.getElementById("corrupted-image-canvas"),
-    reconstructedCanvas = document.getElementById("reconstructed-image-canvas");
+let original = document.getElementById("original-image"),
+    grayscale = document.getElementById("grayscale-image"),
+    corrupted = document.getElementById("corrupted-image"),
+    reconstructed = document.getElementById("reconstructed-image");
+
+let originalCanvas = document.createElement("canvas"),
+    grayscaleCanvas = document.createElement("canvas"),
+    corruptedCanvas = document.createElement("canvas"),
+    reconstructedCanvas = document.createElement("canvas");
 
 let originalCtx = originalCanvas.getContext("2d"),
     grayscaleCtx = grayscaleCanvas.getContext("2d"),
@@ -21,6 +26,10 @@ function reset() {
   image_reconstruction.draw_original_image(grayscaleCtx);
   image_reconstruction.draw_corrupted_image(corruptedCtx);
   image_reconstruction.draw_reconstructed_image(reconstructedCtx);
+
+  grayscale.src = grayscaleCanvas.toDataURL();
+  corrupted.src = corruptedCanvas.toDataURL();
+  reconstructed.src = reconstructedCanvas.toDataURL();
 }
 
 function draw() {
@@ -41,54 +50,30 @@ function draw() {
   reconstructedCanvas.height = h;
 
   reset();
+}
 
-  // let canvas = document.getElementById("original-image-canvas");
-  // canvas.width = this.width;
-  // canvas.height = this.height;
+function gradientStep(i) {
+  let eta = etaInput.value;
+  let steps = parseFloat(stepsPerClickRange.value);
+  console.log(`${i}/${steps}`);
+  if (i >= steps) return;
 
-  // ctx.drawImage(this, 0, 0);
+  image_reconstruction.gradient_step(eta);
 
-  // let data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  // console.log(data);
+  image_reconstruction.draw_reconstructed_image(reconstructedCtx);
 
-  // let probBar = document.getElementById("prob");
-  // let p = parseFloat(probBar.value);
-  // image_reconstruction = wasm.ImageReconstruction.new(data, p);
+  reconstructed = document.getElementById("reconstructed-image");
+  reconstructed.src = reconstructedCanvas.toDataURL();
   
-  // let canvas2 = document.getElementById("grayscale-image-canvas");
-  // canvas2.width = this.width;
-  // canvas2.height = this.height;
-  // let ctx2 = canvas2.getContext("2d");
-  // image_reconstruction.draw_original_image(ctx2);
-
-  // let canvas3 = document.getElementById("corrupted-image-canvas");
-  // canvas3.width = this.width;
-  // canvas3.height = this.height;
-  // let ctx3 = canvas3.getContext("2d");
-  // image_reconstruction.draw_corrupted_image(ctx3);
-
-  // let canvas4 = document.getElementById("reconstructed-image-canvas");
-  // canvas4.width = this.width;
-  // canvas4.height = this.height;
-  // let ctx4 = canvas4.getContext("2d");
-  // image_reconstruction.draw_reconstructed_image(ctx4);
-}
-
-let button = document.getElementById("gradient-step");
-button.onclick = function(ev) {
-  let stepsPerClickBtn = document.getElementById("steps-per-click");
-  let etaField = document.getElementById("eta");
-  let eta = parseFloat(etaField.value);
-  console.log("eta =", eta);
-  console.log(stepsPerClickBtn.value);
-  let canvas4 = document.getElementById("reconstructed-image-canvas");
-  let ctx4 = canvas4.getContext("2d");
-  for (let i = 0; i < stepsPerClickBtn.value; i++) {
-    console.log(`${i + 1}/${stepsPerClickBtn.value}`);
-    image_reconstruction.gradient_step(eta);
+  reconstructed.onload = () => {
+    gradientStep(i + 1);
   }
-  image_reconstruction.draw_reconstructed_image(ctx4);
 }
+let gradientStepBtn = document.getElementById("gradient-step-btn");
+let stepsPerClickRange = document.getElementById("steps-per-click");
+let etaInput = document.getElementById("eta");
+
+gradientStepBtn.onclick = () => gradientStep(0);
 
 let resetBtn = document.getElementById("reset-btn");
 resetBtn.onclick = reset;
@@ -97,9 +82,13 @@ function failed() {
   console.error("The provided file couldn't be loaded as an Image media");
 }
 
-document.getElementById("image-input").onchange = function(event) {
-  let img = new Image();
-  img.onload = draw;
-  img.onerror = failed;
-  img.src = URL.createObjectURL(this.files[0]);
-};
+function loadImage() {
+  original.onload = draw;
+  original.onerror = failed;
+  original.src = URL.createObjectURL(this.files[0]);
+}
+let imageInput = document.getElementById("image-input");
+imageInput.onchange = loadImage;
+if (imageInput.files.length > 0) {
+  loadImage.bind(imageInput)();
+}
